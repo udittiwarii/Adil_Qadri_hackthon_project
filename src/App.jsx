96be27466b85362a5421c03d68efd371a482cf7a
Udit tiwari <tiwariudit0310@gmail.com>
2025-07-16 22:54:56 +0530

    .

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "./Components/Home"
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="animation-div"></div>
+        <h1 className="beforeafter"> is animation</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+    <div>kai beta</div>
+  )
+}
+
+export default Home
